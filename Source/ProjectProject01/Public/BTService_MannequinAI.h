e8998c9929a754e725d19743fd86f7e64b1c4497
BAEK GYEONGCHEOL <bkctwo@tukorea.ac.kr>
2026-09-09 01:01:22 +0900

    AI 가 시야에 따라 무조건 쫓아오도록 설정

--- Source/ProjectProject01/Public/BTService_MannequinAI.h
+++ Source/ProjectProject01/Public/BTService_MannequinAI.h
@@ -19,5 +19,11 @@ public:
 
 protected:
 	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
-	
+
+private:
+	// 플레이어의 화면에 Mannequin 이 확인이 되었는지 여부를 저장한다.
+	bool PlayerSeeMannequin;
+
+	// Mannequin 이 플레이어를 발견했는지 저장한다.
+	bool MannequinSeePlayer;
 };
